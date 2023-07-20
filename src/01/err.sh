4d87be82d346ee66eb463729d9d94a6d4b758bdd
darrpama <darrpama@student.21-school.ru>
2023-07-20 19:06:12 +0000

    init commit

---
+++ src/01/err.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# There is function to print errors into the STDERR (&2)
+#
+# if ! do_something; then
+#   err "Unable to do_something"
+#   exit 1
+# fi
+
+err() {
+  echo "$*" >&2
+  #echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
