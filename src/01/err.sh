0c2f6b37f8ba13a2fb33d3f55bdd45cdb5ed856d
darrpama <darrpama@student.21-school.ru>
2023-07-22 19:07:09 +0000

    saved state

--- src/01/err.sh
+++ src/01/err.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 #
-# There is function to print errors into the STDERR (&2)
-#
-# if ! do_something; then
-#   err "Unable to do_something"
-#   exit 1
-# fi
+# There are functions to check params and print errors into the STDERR (&2)
 
-err() {
-  echo "$*" >&2
-  #echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+
+function print_err {
+	case $1 in
+	1) echo "Скрипт принимает 6 параметров: [Абсолютный путь (/opt/test)] [Количество вложенных папок (1-99)] [Латинские буквы в названии папок, не более 7 (a-z)] [Количество вложенных файлов (1-99)] [Латинские буквы в названии файлов, не более 7 для имени, не более 3 для расширения (a-z.a-z)] [Размер файлов в килобайтах, но не более 100 (3kb)]" >&2;;
+	2) echo "Некорректный абсолютный путь" >&2;;
+	3) echo "Некорректный абсолютный путь" >&2;;
+	4) echo "Некорректный абсолютный путь" >&2;;
+	esac
+	
+}
+
+
+function check_number_of_params {
+	output=1
+	if [[ $1 != 6 ]]
+	then
+		print_error "1"
+		echo "0"
+	else
+		echo "1"
+	fi
 }
